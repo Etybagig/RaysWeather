be3425162e11ad6c5bea4a521f486a8591937d3c
Seth Hobson <wshobson@gmail.com>
2011-03-26 00:20:40 -0400

    Added warnings view controller and xib

---
+++ RaysWeather/WarningsViewController.h
@@ -0,0 +1,16 @@
+//
+//  WarningsViewController.h
+//  RaysWeather
+//
+//  Created by Seth Hobson on 3/26/11.
+//  Copyright 2011 Appalachian State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface WarningsViewController : UIViewController {
+    
+}
+
+@end
---
+++ RaysWeather/WarningsViewController.m
@@ -0,0 +1,15 @@
+//
+//  WarningsViewController.m
+//  RaysWeather
+//
+//  Created by Seth Hobson on 3/26/11.
+//  Copyright 2011 Appalachian State University. All rights reserved.
+//
+
+#import "WarningsViewController.h"
+#import "MyXMLParser.h"
+
+@implementation WarningsViewController
+
+
+@end
