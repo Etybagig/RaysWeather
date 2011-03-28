e02df7f14b9459ba2d9d73e8c7ab1c23d475fafe
Bobby Lunceford <luncefordbr@gmail.com>
2011-03-28 18:32:35 -0400

    finished up some table view stuff on the warnings view

--- RaysWeather/WarningsViewController.m
+++ RaysWeather/WarningsViewController.m
@@ -65,7 +65,10 @@
     return cell;
 }
 
-
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 - (NSString *)addEntry:(NSString *)warning dictionary:(NSMutableDictionary *)dict
 {
